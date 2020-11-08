2435040f6d87adf1d80354512627b6e6fbab0a3b
zulfikar18 <zulfikar.zulfikar@quipper.com>
2020-11-09 05:02:59 +0700

    install glide as a library put image view on list item layout load image by glide set default image

--- app/src/main/java/comtest/ct/cd/zulfikar/user/UserListActivity.kt
+++ app/src/main/java/comtest/ct/cd/zulfikar/user/UserListActivity.kt
@@ -1,7 +1,9 @@
 package comtest.ct.cd.zulfikar.user
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import comtest.ct.cd.zulfikar.R
 import comtest.ct.cd.zulfikar.user.ui.UserListFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +14,13 @@ class UserListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_list_activity)
+        window?.decorView?.apply {
+            systemUiVisibility = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            } else {
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            }
+        }
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, UserListFragment.newInstance())
--- app/src/main/java/comtest/ct/cd/zulfikar/user/ui/UserListViewHolder.kt
+++ app/src/main/java/comtest/ct/cd/zulfikar/user/ui/UserListViewHolder.kt
@@ -2,11 +2,14 @@ package comtest.ct.cd.zulfikar.user.ui
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import comtest.ct.cd.zulfikar.schema.Items
 import kotlinx.android.synthetic.main.v_user_item.view.*
 
 class UserListViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
     fun bind(user: Items) {
         view.txtTitle.text = user.login
+        Glide.with(view.context).load(user.avatarUrl)
+            .placeholder(android.R.drawable.ic_menu_gallery).into(view.avatar)
     }
 }
